ce3c623f23160a62b3433955cdd304dedd2d1deb
taufiqdp <topexdw@gmail.com>
2024-09-09 14:39:21 +0700

    update: remove container margin x

--- components/navbar.jsx
+++ components/navbar.jsx
@@ -14,7 +14,7 @@ export default function Navbar() {
 
   return (
     <nav className="md:px-20 px-2 fixed top-0 w-full bg-neutral-900 text-neutral-50 shadow-md dark:bg-neutral-50 dark:text-neutral-900">
-      <div className="container mx-auto px-4">
+      <div className="container mx-auto">
         <div className="flex items-center justify-between h-16">
           <div className="flex items-center">
             <Link href="/" className="text-xl font-bold">
