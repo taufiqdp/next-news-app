3069d6e997cceef80c612fa25c2b0345dc557bfa
taufiqdp <topexdw@gmail.com>
2024-09-09 14:07:46 +0700

    update: remove bg color

--- components/not-found.jsx
+++ components/not-found.jsx
@@ -3,7 +3,7 @@ import { Button } from "@/components/ui/button";
 
 export function NotFound({ message }) {
   return (
-    <div className="flex flex-col items-center justify-center min-h-screen bg-white text-neutral-950 dark:bg-neutral-950 dark:text-neutral-50">
+    <div className="flex flex-col items-center justify-center min-h-screen text-neutral-950 ">
       <h1 className="text-4xl font-bold mb-4">404</h1>
       <h2 className="text-2xl mb-4">Page Not Found</h2>
       <p className="text-neutral-500 mb-8 dark:text-neutral-400">{message}</p>
