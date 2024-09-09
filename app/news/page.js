a589a538d32c1edc0e57301a705ea13f5c24b233
taufiqdp <topexdw@gmail.com>
2024-09-09 14:42:44 +0700

    update: add heading 1

--- app/news/page.js
+++ app/news/page.js
@@ -3,5 +3,10 @@ import NewsCards from "@/components/news-cards";
 import { DUMMY_NEWS } from "@/dummy-data";
 
 export default function NewsPage() {
-  return <NewsCards newsItems={DUMMY_NEWS} />;
+  return (
+    <>
+      <h1 className="text-3xl font-bold mb-6 text-center">Latest News</h1>
+      <NewsCards newsItems={DUMMY_NEWS} />
+    </>
+  );
 }
